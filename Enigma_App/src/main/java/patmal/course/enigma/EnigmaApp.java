9f3ceaa56237807e0ad4934041ce4271e05ed520
Yarden <yardenvaknin23@gmail.com>
2026-02-20 15:56:41 +0200

    starting migration - spring boot

---
+++ Enigma_App/src/main/java/patmal/course/enigma/EnigmaApp.java
@@ -0,0 +1,10 @@
+package patmal.course.enigma;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnigmaApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EnigmaApp.class, args);
+    }
+}
